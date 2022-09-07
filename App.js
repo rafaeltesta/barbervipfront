5b6d8b22f09c2b835fcf1450f3c637a062fd47b7
Rafael Testa <rafaelmax.testa@gmail.com>
2022-09-07 16:07:39 -0300

    Tela basica cadastro de serviços

--- App.js
+++ App.js
@@ -4,15 +4,17 @@ import CadastroUser from './pages/componentes/CadastroUser';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator  } from '@react-navigation/native-stack';
+import CadastroServico from './pages/componentes/CadastroServico';
 
 const Stack = createNativeStackNavigator ();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Login'>
+      <Stack.Navigator initialRouteName='Cadastro de serviço'>
         <Stack.Screen name="Login" component={Singnin} />
         <Stack.Screen name="Cadastre-se" component={CadastroUser} />
+        <Stack.Screen name='Cadastro de serviço' component={CadastroServico}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
