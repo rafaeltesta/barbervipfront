285c087dbac26fe634560245754c609d00c74d48
Henrique Silva <henrique.195578@edu.unipar.br>
2022-10-11 23:18:12 -0300

    Download splash.json and include animation in the app

--- pages/SplashAnimated/index.js
+++ pages/SplashAnimated/index.js
@@ -1,6 +1,6 @@
 import React  from 'react';
 import { Button, StyleSheet,Text, View } from 'react-native';
-/* Instalado a biblioteca - Lottie -> "npx expo install lottie-react-native" */
+/* Instalado a biblioteca - Lottie -> npx expo install lottie-react-native */
 /* Uso para splah's animadas segue o link: https://lottiefiles.com  */
 import LottieView from "lottie-react-native";
 import { NavigationContainer, useNavigationContainerRef } from '@react-navigation/native';
@@ -15,8 +15,11 @@ export default function Splash() {
       <LottieView
         source={require('../../assets/face.json')}
         autoPlay={true}
-        loop={true}
-        onAnimationFinish={() => NavigationContainer.navigate('../SignIn/Login')}
+        loop={false}
+        style={{width: 400, height: 400}}
+        resizeMode="cover"
+        /* Precisa de ajustar na rota da Splash */
+        onAnimationFinish={() => NavigationContainer.navigate('../routes/AuthRoutes')}
       />
     </View>
   );
